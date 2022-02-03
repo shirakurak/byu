d6821bbc15f6c1fbc0e0e9b8587c63bc342cf0ef
Kenichi SHIRAKURA <surakura@hotmail.co.jp>
2022-02-03 17:28:55 +0900

    newName add

--- foundation/js/main.js
+++ foundation/js/main.js
@@ -4,12 +4,19 @@
   var vm = new Vue({
     el: '#app',
     data: {
+      newName: '',
       name : 'shirakura', 
       names: [
         'name 1',
         'name 2',
         'name 3'
       ]
+    },
+    methods: {
+      addName: function() {
+        this.names.push(this.newName);
+        this.newName = '';
+      }
     }
   });
 })();
